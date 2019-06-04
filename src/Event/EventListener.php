4752a7755f0e80990aaed2032cc50e4d514ae9fc
Jakub Giminski <jakub@gigabyte.software>
2019-06-04 15:26:23 +0100

    Add EventListener interface

---
+++ src/Event/EventListener.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Comquer\Event\EventListener;
+
+use Comquer\DomainIntegration\Event\Event;
+use Comquer\DomainIntegration\NamedResource;
+
+interface EventListener extends NamedResource
+{
+    public function __invoke(Event $event) : void;
+}

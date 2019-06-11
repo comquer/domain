a63ae8750a122a4482f7275a245e0ab3bc4541ae
Jakub Giminski <jakub@gigabyte.software>
2019-06-11 15:27:37 +0100

    Make EventQueueItem not deserializable

--- src/Event/Queue/EventQueueItem.php
+++ src/Event/Queue/EventQueueItem.php
@@ -3,10 +3,9 @@
 namespace Comquer\DomainIntegration\Event\Queue;
 
 use Comquer\DomainIntegration\Event\Event;
-use Comquer\DomainIntegration\Serialization\Deserializable;
 use Comquer\DomainIntegration\Serialization\Serializable;
 
-interface EventQueueItem extends Serializable, Deserializable
+interface EventQueueItem extends Serializable
 {
     public function getEvent() : Event;
 

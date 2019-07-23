c4758051bf83f8de1b48693c81f06ce1903f5be8
Jakub Giminski <jakub@gigabyte.software>
2019-07-23 12:48:24 +0100

    Add methods to event store

--- src/Event/Store/EventStore.php
+++ src/Event/Store/EventStore.php
@@ -2,14 +2,21 @@
 
 namespace Comquer\DomainIntegration\Event\Store;
 
+use Comquer\DomainIntegration\Event\AggregateId;
+use Comquer\DomainIntegration\Event\AggregateType;
 use Comquer\DomainIntegration\Event\Event;
-use Comquer\DomainIntegration\Event\Store\EventId;
 
 interface EventStore
 {
     public function persist(Event $event) : EventId;
 
     public function getById(EventId $eventId) : Event;
+
+    public function getMany(array $query);
+
+    public function getByAggregateId(AggregateId $aggregateId);
+
+    public function getByAggregateType(AggregateType $aggregateType);
 
     public function exists(EventId $eventId) : bool;
 }
